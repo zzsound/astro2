b53e4338aba452b45458d1a2252e6e877318602f
1 <a1@MacBook-Air-de-1.local>
2025-04-06 22:44:50 +0200

    0

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://zzsound.github.io', // o tu dominio personalizado
+});
